f59cc93ed46688c74404800ba6b4f0784b431541
The Bounzer <thebounzer@email.it>
2015-04-06 21:04:41 +0200

    Minor modifications

--- test/spec/kmlparser_spec.rb
+++ test/spec/kmlparser_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Kmlparser, "Basic features" do 
 	it 'Must return 4 points using the test file' do
-		kml=Kmlparser.new(File.open('./KML_Samples.kml'))
+		kml=Kmlparser::Parser.new(File.open('../templates/KML_Samples.kml'))
 		expect(kml.placemarks.count).to eq(4)
 	end
 
