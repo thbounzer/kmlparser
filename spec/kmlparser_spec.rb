2e520e1666f8099b93c3f6fef479c6a97736bb17
thbounzer <carloiannaccone@gmail.com>
2015-04-07 22:58:53 +0200

    Minor changes

--- spec/kmlparser_spec.rb
+++ spec/kmlparser_spec.rb
@@ -1,11 +1,18 @@
 require_relative 'spec_helper'
 
-test_file = 'templates/KML_Samples.kml' 
+test_file = 'templates/KML_Samples.kml'
+kml = Kmlparser::Parser.new(File.open(test_file))
 
-describe Kmlparser, "Basic features" do 
+describe Kmlparser do 
 	it 'Must return 4 points using the test file' do
-		kml=Kmlparser::Parser.new(File.open(test_file))
 		expect(kml.placemarks.count).to eq(4)
 	end
+	
+	it 'Must parse correct coordinates' do 
+		expect(kml.placemarks[2].coordinates[:lat]).to eq(-122.0857667006183)
+	end
 
+	it 'Must return correct address' do 
+		pending
+	end
 end
